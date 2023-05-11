e2735b6a02d0492d89e84bbcaca31c64c3b488d1
mammadli <gulnar.mammadli@ut.ee>
2023-05-11 14:18:45 +0300

    Update makeDecision endpoint with Kafka implementation

--- src/main/java/com/group11/msdecide/service/DecisionService.java
+++ src/main/java/com/group11/msdecide/service/DecisionService.java
@@ -24,9 +24,10 @@ public class DecisionService {
     private final LoanDecisionRepository decisionRepository;
 
     private final KafkaTemplate<String, LoanDto> kafkaTemplate;
-
     @KafkaListener(topics = "loanCreatedTopic", groupId = "loanEventGroup")
     public LoanDecision makeDecision(LoanDto loanDto) {
+
+        log.info("Log message - received from loan topic: {} ", loanDto.toString());
 
         LoanDecision decision = LoanDecision.builder()
                 .loanId(loanDto.getId())
@@ -40,15 +41,23 @@ public class DecisionService {
         int i = decision.getDecidedAmount().compareTo(loanDto.getAmount());
         if (decision.getDecisionStatus() == DecisionStatus.APPROVED && i==0) {
             loanDto.setStatus(Status.ACCEPTED);
+            loanDto.setUpdatedAt(LocalDateTime.now());
+            log.info("Log message - a loan status for loan id: {} has been accepted successfully", loanDto.toString());
             kafkaTemplate.send("decisionTopic", loanDto);
 
         } else if (decision.getDecisionStatus() == DecisionStatus.APPROVED && i>0) {
             loanDto.setStatus(Status.OFFERED);
+            loanDto.setUpdatedAt(LocalDateTime.now());
+            log.info("Log message - a loan status for loan id: {} has been offered successfully", loanDto.toString());
+
             kafkaTemplate.send("decisionTopic", loanDto);
 
 
         } else if (decision.getDecisionStatus() == DecisionStatus.REJECTED) {
             loanDto.setStatus(Status.REFUSED);
+            loanDto.setUpdatedAt(LocalDateTime.now());
+            log.info("Log message - a loan status for loan id: {} has been refused", loanDto.toString());
+
             kafkaTemplate.send("decisionTopic", loanDto);
 
         }
