35880521e47897cbb3168c3761d678413b9d5dc6
mammadli <gulnar.mammadli@ut.ee>
2023-05-11 23:50:37 +0300

    Remove time fields

--- src/main/java/com/group11/msdecide/model/dto/LoanDto.java
+++ src/main/java/com/group11/msdecide/model/dto/LoanDto.java
@@ -12,7 +12,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 
 
@@ -38,9 +37,4 @@ public class LoanDto {
     @Enumerated(EnumType.STRING)
     private Status status;
 
-    @JsonProperty("created_at")
-    private LocalDateTime createdAt;
-
-    @JsonProperty("updated_at")
-    private LocalDateTime updatedAt;
 }
