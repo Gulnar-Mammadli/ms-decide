ac2913543cf7869b1af9cceeb3ed75ff3492693f
mammadli <gulnar.mammadli@ut.ee>
2023-05-12 16:25:09 +0300

    Add financialInfo

--- src/main/java/com/group11/msdecide/controller/DecisionController.java
+++ src/main/java/com/group11/msdecide/controller/DecisionController.java
@@ -1,7 +1,9 @@
 package com.group11.msdecide.controller;
 
 
+import com.group11.msdecide.model.FinancialInformation;
 import com.group11.msdecide.model.LoanDecision;
+import com.group11.msdecide.model.dto.FinancialInfoDto;
 import com.group11.msdecide.model.dto.LoanDto;
 import com.group11.msdecide.service.DecisionService;
 import lombok.RequiredArgsConstructor;
@@ -22,5 +24,15 @@ public class DecisionController {
     @GetMapping("/{loanId}")
     LoanDecision getDecision(@PathVariable Long loanId){
         return decisionService.getDecision(loanId);
+    }
+
+    @PostMapping("/finance")
+    FinancialInformation addFinancialInfo(@RequestBody FinancialInfoDto infoDto){
+        return decisionService.addFinancialInfo(infoDto);
+    }
+
+    @GetMapping("/{customerId}")
+    FinancialInformation getFinancialInfo(@PathVariable Long customerId){
+        return decisionService.getFinancialInfo(customerId);
     }
 }
--- src/main/java/com/group11/msdecide/service/DecisionService.java
+++ src/main/java/com/group11/msdecide/service/DecisionService.java
@@ -1,9 +1,12 @@
 package com.group11.msdecide.service;
 
+import com.group11.msdecide.model.FinancialInformation;
 import com.group11.msdecide.model.LoanDecision;
+import com.group11.msdecide.model.dto.FinancialInfoDto;
 import com.group11.msdecide.model.dto.LoanDto;
 import com.group11.msdecide.model.enums.DecisionStatus;
 import com.group11.msdecide.model.enums.Status;
+import com.group11.msdecide.repository.FinancialInfoRepository;
 import com.group11.msdecide.repository.LoanDecisionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +25,7 @@ import java.util.Optional;
 public class DecisionService {
 
     private final LoanDecisionRepository decisionRepository;
+    private final FinancialInfoRepository financialInfoRepository;
 
     private final KafkaTemplate<String, LoanDto> kafkaTemplate;
 
@@ -39,32 +43,81 @@ public class DecisionService {
                 .decidedAmount(BigDecimal.valueOf(1000))
                 .build();
 
-        int i = decision.getDecidedAmount().compareTo(loanDto.getAmount());
-        if (decision.getDecisionStatus() == DecisionStatus.APPROVED && i == 0) {
-            loanDto.setStatus(Status.ACCEPTED);
-            log.info("Log message - a loan status for loan id: {} has been accepted successfully", loanDto.toString());
-            kafkaTemplate.send("decisionTopic", loanDto);
+//        FinancialInformation financialInfo = getFinancialInfo(loanDto.getCustomerId());
+//        if (financialInfo != null) {
+//            log.info("Log message - making decision based on financial information: {} ", loanDto.toString());
+//            BigDecimal monthLyIncome = financialInfo.getAnnualIncome().divide(BigDecimal.valueOf(12));
+//            if (financialInfo.getMonthlyTurnover().compareTo(monthLyIncome) == 1) {
+//                BigDecimal difference = financialInfo.getMonthlyTurnover().subtract(monthLyIncome);
+//                if(difference.compareTo(BigDecimal.ZERO)>0){
+//                decision.setDecidedAmount(difference);
+//                decision.setDecisionStatus(DecisionStatus.APPROVED);
+//            }else {
+//                    decision.setDecisionStatus(DecisionStatus.REJECTED);
+//                }
+//            }
 
-        } else if (decision.getDecisionStatus() == DecisionStatus.APPROVED && i > 0) {
-            loanDto.setStatus(Status.OFFERED);
-            log.info("Log message - a loan status for loan id: {} has been offered successfully", loanDto.toString());
 
-            kafkaTemplate.send("decisionTopic", loanDto);
+            int i = decision.getDecidedAmount().compareTo(loanDto.getAmount());
+            if (decision.getDecisionStatus() == DecisionStatus.APPROVED && i == 0) {
+                loanDto.setStatus(Status.ACCEPTED);
+                loanDto.setDecidedAmount(decision.getDecidedAmount());
+                log.info("Log message - a loan status for loan id: {} has been accepted successfully", loanDto.toString());
+                kafkaTemplate.send("decisionTopic", loanDto);
+
+            } else if (decision.getDecisionStatus() == DecisionStatus.APPROVED && i <0) {
+                loanDto.setStatus(Status.OFFERED);
+                loanDto.setDecidedAmount(decision.getDecidedAmount());
+                log.info("Log message - a loan status for loan id: {} has been offered successfully", loanDto.toString());
+
+                kafkaTemplate.send("decisionTopic", loanDto);
 
 
-        } else if (decision.getDecisionStatus() == DecisionStatus.REJECTED) {
-            loanDto.setStatus(Status.REFUSED);
-            log.info("Log message - a loan status for loan id: {} has been refused", loanDto.toString());
+            } else if (decision.getDecisionStatus() == DecisionStatus.APPROVED && i >0) {
+                loanDto.setStatus(Status.OFFERED);
+                loanDto.setDecidedAmount(decision.getDecidedAmount());
+                log.info("Log message - a loan status for loan id: {} has been offered successfully", loanDto.toString());
+                kafkaTemplate.send("decisionTopic", loanDto);
 
-            kafkaTemplate.send("decisionTopic", loanDto);
+            } else if (decision.getDecisionStatus() == DecisionStatus.REJECTED) {
+                loanDto.setStatus(Status.REFUSED);
+                log.info("Log message - a loan status for loan id: {} has been refused", loanDto.toString());
+
+                kafkaTemplate.send("decisionTopic", loanDto);
+
+            }
+            return decisionRepository.save(decision);
 
         }
-        return decisionRepository.save(decision);
+//        return null;
 
-    }
+//    }
 
     public LoanDecision getDecision(Long loanId) {
         Optional<LoanDecision> result = decisionRepository.findByLoanId(loanId);
         return result.orElse(null);
+    }
+
+    public FinancialInformation addFinancialInfo(FinancialInfoDto infoDto) {
+        Optional<FinancialInformation> info = financialInfoRepository.findByCustomerId(infoDto.getCustomerId());
+        if (info.isEmpty()) {
+            FinancialInformation information = FinancialInformation.builder()
+                    .customerId(infoDto.getCustomerId())
+                    .monthlyTurnover(infoDto.getMonthlyTurnover())
+                    .annualIncome(infoDto.getAnnualIncome())
+                    .annualAverageBalance(infoDto.getAnnualAverageBalance())
+                    .currentBalance(infoDto.getCurrentBalance())
+                    .currentDueDays(infoDto.getCurrentDueDays())
+                    .numberOfDueDays(infoDto.getNumberOfDueDays())
+                    .createdAt(LocalDateTime.now())
+                    .build();
+            return financialInfoRepository.save(information);
+        }
+        return null;
+    }
+
+    public FinancialInformation getFinancialInfo(Long customerId) {
+        Optional<FinancialInformation> info = financialInfoRepository.findByCustomerId(customerId);
+        return info.orElse(null);
     }
 }
