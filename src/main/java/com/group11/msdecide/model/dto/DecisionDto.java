e380f419f8def01f551ebac20543e9f578f65d2d
mammadli <gulnar.mammadli@ut.ee>
2023-05-15 01:22:07 +0300

    Rename and add new fields

--- src/main/java/com/group11/msdecide/model/dto/DecisionDto.java
+++ src/main/java/com/group11/msdecide/model/dto/DecisionDto.java
@@ -1,19 +1,25 @@
 package com.group11.msdecide.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.group11.msdecide.model.enums.DecisionStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
-public class LoanDecisionDto {
+public class DecisionDto {
 
     @JsonProperty("customer_id")
     private Long customerId;
@@ -23,5 +29,15 @@ public class LoanDecisionDto {
 
     @JsonProperty("decided_amount")
     private BigDecimal decidedAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DecisionStatus decisionStatus;
+
+
+    @JsonProperty("decision_date")
+    private LocalDate decisionDate;
+
+
 
 }
